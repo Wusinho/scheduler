27db795ed020996f101448bf4457aeb8eacf9653
Heber Lazo <heberlazo@gmail.com>
2022-09-23 19:57:49 -0500

    Added first counter

--- ror/app/models/user.rb
+++ ror/app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, :case_sensitive => true
   validates_format_of :email, with: /@/
   validates :password_digest, :username, :name, :last_name, presence: true
+
+  before_save :add_first_sign_in_counter
+
+  def add_first_sign_in_counter
+    self.sign_in_count = 1
+  end
+
+
 end
