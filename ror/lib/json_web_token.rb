99f0de0f50da8893094a101060d4343bc415aadb
Heber Lazo <heberlazo@gmail.com>
2022-09-23 19:01:18 -0500

    create jsonwebtoken

--- ror/lib/json_web_token.rb
+++ ror/lib/json_web_token.rb
@@ -1,3 +1,13 @@
+# create a JsonWebToken
 class JsonWebToken
+  SECRET_KEY = ENV['SECRET_KEY_BASE']
 
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    JWT.decode(token, SECRET_KEY)
+  end
 end
