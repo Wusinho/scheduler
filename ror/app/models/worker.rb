5ea942b094f1d4e3dd4ba8b3d80614ea7f0d35de
Heber Lazo <heberlazo@gmail.com>
2022-09-26 22:55:28 -0500

    bug with worker model association

--- ror/app/models/worker.rb
+++ ror/app/models/worker.rb
@@ -1,7 +1,7 @@
 class Worker < ApplicationRecord
   has_many :departments, dependent: :destroy
 
-  validates :email, presence: true
+  validates :email, :name, :last_name, presence: true
   validates_uniqueness_of :email, case_sensitive: true
   validates_format_of :email, with: /@/
 end
--- ror/spec/models/worker_spec.rb
+++ ror/spec/models/worker_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Worker, type: :model do
   describe 'validations' do
     it 'validate presence of required fields' do
       should validate_presence_of(:email)
+      should validate_presence_of(:name)
+      should validate_presence_of(:last_name)
     end
 
     it 'validate relations' do
